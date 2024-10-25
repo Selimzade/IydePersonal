0a9c9ef3f0527ec5637190e9b927f1ca42ce4986
Selimzade <37342774+Selimzade@users.noreply.github.com>
2024-10-25 17:13:20 +0400

    Add Punkts Controller

---
+++ IydePersonal/IydePersenal/Controllers/PunktsController.cs
@@ -0,0 +1,30 @@
+﻿using IydePersonal.API.Data;
+using IydePersonal.WEB.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace IydePersonal.WEB.Controllers
+{
+    public class PunktsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public PunktsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var punkts = await _context.Punkts.ToListAsync();
+            var punktsVm = punkts.Select(p => new PunktListViewModel
+            {
+                Id = p.Id,
+                Name = p.Name,
+                Point = p.Point,
+            });
+
+            return View(punktsVm);
+        }
+    }
+}
---
+++ IydePersonal/IydePersenal/Models/PunktListViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace IydePersonal.WEB.Models
+{
+    public class PunktListViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Point { get; set; }
+    }
+}
