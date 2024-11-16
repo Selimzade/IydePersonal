a3e202bd11f167189e916dc70bcd68e65b1339ef
Selimzade <37342774+Selimzade@users.noreply.github.com>
2024-11-16 10:22:44 +0400

    Add API Project

--- IydePersonal/IydePersonal.API/Program.cs
+++ IydePersonal/IydePersonal.API/Program.cs
@@ -1,47 +1,25 @@
-using IydePersonal.API.Data;
-using Microsoft.EntityFrameworkCore;
-using System.Reflection;
+var builder = WebApplication.CreateBuilder(args);
 
-namespace IydePersonal.API
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-
-            var config = builder.Configuration;
-
-            builder.Services.AddControllers();
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-            builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(config.GetConnectionString("DefaultConnection")));
-            builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
-            
-          
-
-            var app = builder.Build();
-
-            using (var scope = app.Services.CreateScope())
-            {
-                var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-                db.Database.Migrate();
-            }
-
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
-        }
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
