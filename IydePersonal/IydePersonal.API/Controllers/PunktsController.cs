753b56b29cb31c620e0d22bac2521a3401a242c5
Selimzade <37342774+Selimzade@users.noreply.github.com>
2024-11-16 12:15:27 +0400

    Add Cors

---
+++ IydePersonal/IydePersonal.API/Controllers/PunktsController.cs
@@ -0,0 +1,24 @@
+﻿using IydePersonal.Application.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IydePersonal.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PunktsController : ControllerBase
+    {
+        private readonly IPunktService _punktService;
+
+        public PunktsController(IPunktService punktService)
+        {
+            _punktService = punktService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetPunks()
+        {
+            var punkts = await _punktService.GetPunktListAsync();
+            return Ok(punkts);
+        }
+    }
+}
--- IydePersonal/IydePersonal.API/Program.cs
+++ IydePersonal/IydePersonal.API/Program.cs
@@ -1,20 +1,38 @@
+using IydePersonal.Application;
+using IydePersonal.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+var config = builder.Configuration;
+
+builder.Services.AddInfrastructure(config);
+builder.Services.AddApplication();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors();
 
 app.UseHttpsRedirection();
 
