557bbfef7f2992f98f3bef52acb9cdf792a48bc8
princ <princ@DESKTOP-RIE00ER>
2024-11-21 16:51:35 +0300

    change_index_view

---
+++ IydePersonal/IydePersenal/Areas/Admin/Controllers/PunktController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IydePersonal.WEB.Areas.Admin.Controllers
+{
+    public class PunktController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
